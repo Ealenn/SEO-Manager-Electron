433500ee9759b56f7e9f44f60891bbfbb53e4e86
Marchandise Rudy <r.marchandise@sfr.fr>
2016-12-05 15:25:05 +0100

    Improved progress bar

--- view/res/js/ProgressBar.class.js
+++ view/res/js/ProgressBar.class.js
@@ -6,27 +6,35 @@ class ProgressBar {
   }
 
   changeState(){
+    var html = '';
     var total = 0;
     var nbFinish = 0;
 
+    html += '<ul>';
     $.each(this.Step, function(index, value) {
+      var img = 'loading.gif';
       total++;
-      if(value == 0)
+      if(value == 0){
         nbFinish++;
+        img = 'check.png';
+      }
+
+      /* HTML */
+      html += '<li>' + index + ' <img src="res/img/'+img+'" height="24" width="24">' + '</li>';
     });
+    html += '</ul>';
 
     var prc = 100 - Math.floor((nbFinish / total) * 100);
     $('#' + this.id).attr('style','width: ' + (100 - prc) + '%');
+    $('#' + this.text).html(html);
   }
 
   startStep(stepName){
-    $('#' + this.text).html('Start ' + stepName);
     this.Step[stepName] = 1;
     this.changeState();
   }
 
   finishStep(stepName){
-    $('#' + this.text).html('Finish ' + stepName);
     this.Step[stepName] = 0;
     this.changeState();
   }
